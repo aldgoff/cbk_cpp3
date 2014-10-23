5de0d16a5912accedc4cd4f911d676d5298941c6
aldgoff <aldgoff@ALDGOFF-MOBL1.amr.corp.intel.com>
2014-10-23 14:02:53 -0700

    Eclipse C++ project referring to a local git repository.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[]) {
 	cout << "Hello from Mac vi.\n";
 	cout << "Hello from PC Eclipse Git perspective.\n";
+	cout << "Git project from within Eclipse workspace.\n";
+	cout << "Eclipse C++ project referring to a local git repository.\n";
 	return 0;
 }
 
