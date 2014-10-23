fac39d70244940c6a36d16d370c6bbc80a213b2f
aldgoff <aldgoff@ALDGOFF-MOBL1.amr.corp.intel.com>
2014-10-23 13:37:03 -0700

    Add 'from'.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[]) {
 	cout << "Hello from Mac vi.\n";
 	cout << "Hello from PC Eclipse Git perspective.\n";
-	cout << "Git project within Eclipse workspace.\n";
+	cout << "Git project from within Eclipse workspace.\n";
 	return 0;
 }
 
